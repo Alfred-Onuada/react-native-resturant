7dbbf48ebb7f35b4a9ad0225877937b0638357f2
Alfred-Onuada <aonuada5@gmail.com>
2024-03-18 14:20:19 +0100

    update

--- app/services/admin.ts
+++ app/services/admin.ts
@@ -1,8 +1,8 @@
 import { IFood } from "../interfaces/food";
 import storage from "../utils/storage";
 
-// const base_url = "https://resturant-server-zgh4.onrender.com";
-const base_url = "http://localhost:6777";
+const base_url = "https://resturant-server-zgh4.onrender.com";
+// const base_url = "http://localhost:6777";
 
 export async function deleteItemAPI(id: string) {
   await fetch(base_url + '/foods/' + id, {
